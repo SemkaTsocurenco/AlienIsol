199ffe225ca2067576e9f8d5d5e4101d6199d8fd
Semka_Tsokurenco <semka0tsocurenco@gmail.com>
2023-04-06 01:08:36 +0300

    полное перемещение корабля (право лево). остановка у краев экрана.

--- Alien_Isolation.py
+++ Alien_Isolation.py
@@ -23,13 +23,26 @@ class AlienInvasion():
 			#Отслеживает события на клавиатуре и мышке
 			self._check_ivents()  # проверка на ивенты
 			self._update_screen() #при каждом проходе цикла прописовывается экран
-			
+			self.ship.Update() # обновляет положение корабля
 			
 	def _check_ivents(self):   #вспомогательный класс для run game
 		# ждёт нажатия клавиш и события мышки
 		for event in py.event.get():
 			if event.type == py.QUIT:
 				sys.exit()
+				
+			elif event.type == py.KEYDOWN:
+				if event.key == py.K_d:
+					self.ship.moving_right = True
+				elif event.key == py.K_a:
+					self.ship.moving_left = True
+					
+			elif event.type == py.KEYUP:
+				if event.key == py.K_d:
+					self.ship.moving_right = False
+				elif event.key == py.K_a:
+					self.ship.moving_left = False
+				
 		
 	def _update_screen(self):  #вспомогательный класс для run game
 		# обновляет экран
@@ -38,7 +51,8 @@ class AlienInvasion():
 		# последний прорисованный экран
 		py.display.flip()
 
-
+	
+	
 if __name__ == '__main__':
 	# создать экземпляр
 	ai=AlienInvasion()
--- ship.py
+++ ship.py
@@ -1,22 +1,45 @@
 import pygame as py
 from PIL import Image
 import os
+from settings import Settings
 class Ship():
 	# управление кораблём
 	def __init__(self,ai_game):
-		self.screen = ai_game.screen
-		self.screen_rect = ai_game.screen.get_rect()
+		self.screen = ai_game.screen   # получаем экземпляр экрана
+		self.settings = ai_game.settings # получаем настройки
+
+		self.screen_rect = ai_game.screen.get_rect() # получение коорд экрана
 		
 		#Загрузим изображение корабля
 		self.Image = py.image.load(
 			"C:\\Users\\semn-\\PycharmProjects\\AlienIsol\\images\\new_space_ship.png")
-		self.rect = self.Image.get_rect()
+		self.rect = self.Image.get_rect() # координаты корабля
 		# Появление корабля снизу экрана
-		self.rect.midbottom = self.screen_rect.midbottom
+		self.rect.midbottom = self.screen_rect.midbottom # снизу по центру
+		
+		# Сохраняем вещественную координату центра корабля
+		self.x = float(self.rect.x)
+		
+		#для переещения
+		self.moving_right = False
+		self.moving_left = False
 		
 	def blitme(self):
 		#корабль в текущей позиции
 		self.screen.blit(self.Image, self.rect)
+	
+	def Update(self):
+		# обновление позиции
+		if self.moving_right and self.rect.right<self.screen_rect.right:
+			self.x += self.settings.ship_speed
+		if self.moving_left and self.rect.left>self.screen_rect.left:
+			self.x -= self.settings.ship_speed
+	
+		#обновление rect на основании х
+		self.rect.x = self.x
+	
+	
+	
 	
 def resize_im(path):
 	img=Image.open(path)
