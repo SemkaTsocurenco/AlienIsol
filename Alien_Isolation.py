bec718919789a526f8457e23de89db3179ec5d19
Semka_Tsokurenco <semka0tsocurenco@gmail.com>
2023-04-06 00:02:36 +0300

    упрощение кода, разделение его на сегменты

--- Alien_Isolation.py
+++ Alien_Isolation.py
@@ -4,34 +4,39 @@ import sys
 import pygame as py
 from settings import Settings
 from ship import Ship
-class AlienInvasion:
+class AlienInvasion():
 	
 	def __init__(self):
 		# инициализирует работу класса
 		py.init()
 		self.settings=Settings()
 		
-		self.screen = py.display.set_mode(
-			(self.settings.screen_width,self.settings.screen_height))
+		self.screen = py.display.set_mode((self.settings.screen_width,self.settings.screen_height))
 		py.display.set_caption("Alien Invasion")
-		# Цвет фона
-		self.bg_color = self.settings.bg_color
-		self.ship = Ship()
+		# Прописовка корабля
+		self.ship = Ship(self)
 		
 	
 	def run_game(self):
 		# начинает игру
 		while True:
 			#Отслеживает события на клавиатуре и мышке
-			for event in py.event.get():
-				if event.type == py.QUIT:
-					sys.exit()
-			#при каждом проходе цикла прописовывается экран
-			self.screen.fill(self.settings.bg_color)
-			#self.ship.blitme()
-			#последний прорисованный экран
-			py.display.flip()
+			self._check_ivents()  # проверка на ивенты
+			self._update_screen() #при каждом проходе цикла прописовывается экран
 			
+			
+	def _check_ivents(self):   #вспомогательный класс для run game
+		# ждёт нажатия клавиш и события мышки
+		for event in py.event.get():
+			if event.type == py.QUIT:
+				sys.exit()
+		
+	def _update_screen(self):  #вспомогательный класс для run game
+		# обновляет экран
+		self.screen.fill(self.settings.bg_color)
+		self.ship.blitme()
+		# последний прорисованный экран
+		py.display.flip()
 
 
 if __name__ == '__main__':
--- ship.py
+++ ship.py
@@ -1,5 +1,6 @@
 import pygame as py
-
+from PIL import Image
+import os
 class Ship():
 	# управление кораблём
 	def __init__(self,ai_game):
@@ -7,8 +8,9 @@ class Ship():
 		self.screen_rect = ai_game.screen.get_rect()
 		
 		#Загрузим изображение корабля
-		self.Image = py.image.load("C:\\Users\\semn-\\PycharmProjects\\AlienIsol\\images\\space_ship.png")
-		self.rect = self.Image.get_rect
+		self.Image = py.image.load(
+			"C:\\Users\\semn-\\PycharmProjects\\AlienIsol\\images\\new_space_ship.png")
+		self.rect = self.Image.get_rect()
 		# Появление корабля снизу экрана
 		self.rect.midbottom = self.screen_rect.midbottom
 		
@@ -16,3 +18,9 @@ class Ship():
 		#корабль в текущей позиции
 		self.screen.blit(self.Image, self.rect)
 	
+def resize_im(path):
+	img=Image.open(path)
+	img=img.resize((75,75))
+	img.save(f'{os.path.dirname(path)}\\new_{os.path.basename(path)}')
+	
+resize_im("C:\\Users\\semn-\\PycharmProjects\\AlienIsol\\images\\space_ship.png")
